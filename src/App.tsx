ee228f9eaaf9c314ef98654e8184f904139ec9a7
Alex Bennett <alexandergraybennett@gmail.com>
2022-07-27 15:08:17 -0600

    copy paste from docs and see what happens (#23)
    * copy paste from docs and see what happens
    
    * test: test change
    
    * bugfix: fixes file name
    
    * put workflow in correct dir
    
    * bug: removes bug
    
    * task: fix spacing
    
    * chore: fix spacing again

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { RootStoreProvider } from "./context/stores.context";
 import { NotesTable } from "./features/notesTable/notesTable.component";
 import { CreateNote } from "./features/createNote/createNote.component";
-
 import "./App.css";
 
 function App() {
