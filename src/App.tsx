5eb06f102e2c35853e8ba7cd6b1dc9b574f1bb8a
Alex Bennett <alexandergraybennett@gmail.com>
2022-07-27 15:33:46 -0600

    chore: adds bug

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { RootStoreProvider } from "./context/stores.context";
 import { NotesTable } from "./features/notesTable/notesTable.component";
 import { CreateNote } from "./features/createNote/createNote.component";
 import "./App.css";
-
+// 🐛
 function App() {
   return (
     <RootStoreProvider>
