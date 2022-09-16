d310e3c337b781ad9237508abf918f59ff71fa31
Alex Bennett <alexandergraybennett@gmail.com>
2022-09-15 19:35:27 -0600

    feature 1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { NotesTable } from './features/notesTable/notesTable.component';
 import { CreateNote } from './features/createNote/createNote.component';
 import './App.css';
 
-// adds feature 1
 // adds feat a, iterates on a
 
 // adds feature b. iterates on feat b.
