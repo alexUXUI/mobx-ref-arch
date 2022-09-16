cd024510d679c290909f307f738ccc65b7e6c560
Alex Bennett <alexandergraybennett@gmail.com>
2022-09-15 19:36:02 -0600

    feature 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,6 @@ import { NotesTable } from './features/notesTable/notesTable.component';
 import { CreateNote } from './features/createNote/createNote.component';
 import './App.css';
 
-// adds feat c
-// adds whatev
-
-// new commit
-// new release
-
-// some feature
 function App() {
   return (
     <RootStoreProvider>
