76b53e81c0fa2525b3f22e9a925d19e637040e11
Alex Bennett <alexandergraybennett@gmail.com>
2022-09-15 19:13:10 -0600

    Release (#67)
    * set commit  output
    
    * re-renable slack message
    
    * pass the correct commits
    
    * symbolic change
    
    * branch names
    
    * decrease outputs
    
    * simplify
    
    * simplify
    
    * join
    
    * test
    
    * exec logs
    
    * try shell
    
    * try shell again

--- .github/actions/diff-log-parser/main.js
+++ .github/actions/diff-log-parser/main.js
@@ -1,52 +1,15 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
-const simpleGit = require('simple-git');
 
 async function run() {
   try {
-    const git = simpleGit();
+    // core.setOutput('commits', github.context.payload.commits);
 
-    const tags = await git.tags();
+    const beforeCommit = github.context.payload.before;
+    const afterCommit = github.context.payload.after;
+    const diffString = `${beforeCommit}..${afterCommit}`;
 
-    const previousTag =
-      tags.all.length < 2 ? undefined : tags.all[tags.all.length - 2];
-    const latestTag =
-      tags.all.length < 1 ? undefined : tags.all[tags.all.length - 1];
-
-    const commits = await git.log({
-      from: previousTag,
-      to: latestTag,
-      format: {
-        abbrev: '%h',
-        author: '@%an',
-        message: '%s',
-      },
-      splitter: '\n',
-      multiLine: false,
-    });
-
-    let textLog = '';
-    let markdownLog = '';
-
-    for (const commit of commits.all) {
-      textLog += `${commit.abbrev} - ${commit.author} - ${commit.message}\n`;
-      markdownLog += `[\`${commit.abbrev}\`](${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}/commit/${commit.abbrev}) ${commit.author} - ${commit.message}\n`;
-    }
-
-    core.info(`previousTag: ${previousTag}`);
-    core.info(`latestTag: ${latestTag}`);
-    core.info(textLog);
-
-    core.setOutput('previousTag', previousTag);
-    core.setOutput('latestTag', latestTag);
-    core.setOutput('log', textLog);
-    core.setOutput('markdownLog', markdownLog);
-    core.setOutput('commits', JSON.stringify(github.context.payload.commits));
-
-    /// other appraoch
-
-    // run git log in exec
     const generateLog = async () => {
       let options = {};
       let output = '';
@@ -56,19 +19,6 @@ async function run() {
           output += data.toString();
         },
       };
-
-      const beforeCommit = github.context.payload.before;
-      const afterCommit = github.context.payload.after;
-      console.log(`beforeCommit: ${beforeCommit}`);
-      console.log(`afterCommit: ${afterCommit}`);
-      console.log(
-        `github.context.payload: ${JSON.stringify(github.context.payload)}`
-      );
-      console.log(github.context);
-      core.debug(`beforeCommit: ${beforeCommit}`);
-      core.debug(`afterCommit: ${afterCommit}`);
-      core.debug(github.context.payload);
-      const diffString = `${beforeCommit}..${afterCommit}`;
 
       console.log('COMMITS');
       console.log(github.context.payload.commits);
@@ -80,12 +30,20 @@ async function run() {
 
       await exec.exec(
         'git',
-        ['log', '--pretty=format:%h - %an - %s', diffString],
+        ['log', '--pretty=format:%s', diffString],
         options
       );
 
       core.setOutput('execLog', output);
-      console.log(output);
+      core.debug(`beforeCommit: ${beforeCommit}`);
+      core.debug(`afterCommit: ${afterCommit}`);
+      console.log(
+        `github.context.payload: ${JSON.stringify(github.context.payload)}`
+      );
+      core.debug('execLog');
+      core.debug(output);
+
+      core.setOutput('commits', output);
     };
 
     generateLog();
@@ -95,38 +53,3 @@ async function run() {
 }
 
 run();
-
-// superifical change
-
-// try {
-//   // `who-to-greet` input defined in action metadata file
-//   console.log(`Hello!`);
-//   // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2);
-//   console.log(`The event payload: ${payload}`);
-
-//   const beforeCommit = github.context.payload.before;
-//   const afterCommit = github.context.payload.after;
-
-//   // run git log in exec
-//   const generateLog = async () => {
-//     let options = {};
-//     let output = '';
-//     options.listeners = {
-//       stdout: (data) => {
-//         console.log(data.toString());
-//         output += data.toString();
-//       },
-//     };
-
-//     const diffString = `${afterCommit}...${beforeCommit}`;
-
-//     await exec.exec('git', ['log', '-n 5 --pretty=format:"%s"'], options);
-//     core.setOutput('logs', output);
-//     console.log(output);
-//   };
-
-//   generateLog();
-// } catch (error) {
-//   core.setFailed(error.message);
-// }
